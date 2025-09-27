3225e11f4f5133b6d4b0617744a0baf90fbefe0a
onlypdg6-netizen <onlypdg6@gmail.com>
2025-09-26 23:58:57 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DML-MD",
+SESSION_ID: process.env.SESSION_ID || "DML-MD~mMdzzRZD#_STNe_g6hGzSsMXslsP0WMxAj32rFtbbRShn8caMQ1Y",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -29,17 +29,17 @@ PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "DML-TECH",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "DML-TECH",
+STICKER_NAME: process.env.STICKER_NAME || "ONLY1PDG",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255615752312",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50938673725",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "DML",
+OWNER_NAME: process.env.OWNER_NAME || "PDG",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© Powered by DML*",
 // add bot owner name    
